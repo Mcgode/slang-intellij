be3c5128acf46ac1e39bb2383082c51020393e7b
GitHub Action <action@github.com>
2024-11-30 09:06:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/mcgode/slangintellij/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/mcgode/slangintellij/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.mcgode.slangintellij.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
