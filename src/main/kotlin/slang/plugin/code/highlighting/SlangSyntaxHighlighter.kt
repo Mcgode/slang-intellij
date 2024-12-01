8138cc3413f8433a41989e6e14f4aead660abe33
maxgodefroy <max@godefroy.net>
2024-12-01 13:38:02 +0100

    Code syntax highlight changes

--- src/main/kotlin/slang/plugin/code/highlighting/SlangSyntaxHighlighter.kt
+++ src/main/kotlin/slang/plugin/code/highlighting/SlangSyntaxHighlighter.kt
@@ -16,12 +16,19 @@ class SlangSyntaxHighlighter: SyntaxHighlighterBase() {
 
     object SlangTextAttributes {
         val SLANG_VARIABLE_TEXT = createTextAttributesKey("Slang.Variable", IDENTIFIER)
-        val SLANG_USER_DEFINED_TYPE_TEXT = createTextAttributesKey("Slang.UserDefinedType", CLASS_NAME)
+        val SLANG_USER_DEFINED_TYPE_TEXT = createTextAttributesKey("Slang.UserDefinedType", CLASS_REFERENCE)
         val SLANG_BUILTIN_NAME_TEXT = createTextAttributesKey("Slang.BuiltinName", KEYWORD)
         val SLANG_KEYWORD_TEXT = createTextAttributesKey("Slang.Keyword", KEYWORD)
-        val SLANG_NUMERIC_LITERALS_TEXT = createTextAttributesKey("Slang.Keyword", NUMBER)
+        val SLANG_NUMERIC_LITERALS_TEXT = createTextAttributesKey("Slang.NumericLiteral", NUMBER)
+        val SLANG_STRING_LITERAL_TEXT = createTextAttributesKey("Slang.StringLiteral", STRING)
         val SLANG_LINE_COMMENT_TEXT = createTextAttributesKey("Slang.LineComment", LINE_COMMENT)
         val SLANG_MULTILINE_COMMENT_TEXT = createTextAttributesKey("Slang.MultilineComment", BLOCK_COMMENT)
+        val SLANG_BRACE_TEXT = createTextAttributesKey("Slang.Brace", BRACES)
+        val SLANG_BRACKET_TEXT = createTextAttributesKey("Slang.Bracket", BRACKETS)
+        val SLANG_COMMA_TEXT = createTextAttributesKey("Slang.Comma", COMMA)
+        val SLANG_DOT_TEXT = createTextAttributesKey("Slang.Dot", DOT)
+        val SLANG_PAREN_TEXT = createTextAttributesKey("Slang.Parentheses", PARENTHESES)
+        val SLANG_SEMICOLON_TEXT = createTextAttributesKey("Slang.Semicolon", SEMICOLON)
         val SLANG_BAD_CHARACTER_TEXT = createTextAttributesKey("Slang.BadCharacter", HighlighterColors.BAD_CHARACTER)
     }
 
@@ -32,11 +39,17 @@ class SlangSyntaxHighlighter: SyntaxHighlighterBase() {
     private fun mapTokenToTextAttribute(tokenType: IElementType): TextAttributesKey? {
         return when (tokenType) {
             in (SlangTokenSets.VARIABLES) -> SlangTextAttributes.SLANG_VARIABLE_TEXT
+            SlangTypes.USER_TYPE_NAME -> SlangTextAttributes.SLANG_USER_DEFINED_TYPE_TEXT
             in (SlangTokenSets.BUILTINS) -> SlangTextAttributes.SLANG_BUILTIN_NAME_TEXT
             in (SlangTokenSets.KEYWORDS) -> SlangTextAttributes.SLANG_KEYWORD_TEXT
             in (SlangTokenSets.NUMERIC_LITERALS) -> SlangTextAttributes.SLANG_NUMERIC_LITERALS_TEXT
+            SlangTypes.STRING_LITERAL -> SlangTextAttributes.SLANG_STRING_LITERAL_TEXT
+            SlangTypes.BOOL_LITERAL -> SlangTextAttributes.SLANG_KEYWORD_TEXT
             SlangTypes.LINE_COMMENT -> SlangTextAttributes.SLANG_LINE_COMMENT_TEXT
             SlangTypes.MULTILINE_COMMENT -> SlangTextAttributes.SLANG_MULTILINE_COMMENT_TEXT
+            SlangTypes.COMMA -> SlangTextAttributes.SLANG_COMMA_TEXT
+//            SlangTypes.DOT -> SlangTextAttributes.SLANG_DOT_TEXT
+            SlangTypes.SEMICOLON -> SlangTextAttributes.SLANG_SEMICOLON_TEXT
             TokenType.BAD_CHARACTER -> SlangTextAttributes.SLANG_BAD_CHARACTER_TEXT
             else -> null
         }
