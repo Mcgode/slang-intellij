2d34f9abc2ee08f728d67f86260187247713f1e7
maxgodefroy <max@godefroy.net>
2024-12-23 19:03:59 +0100

    Direct mapping

--- src/main/kotlin/slang/plugin/code/highlighting/SlangSemanticTokenColorsProvider.kt
+++ src/main/kotlin/slang/plugin/code/highlighting/SlangSemanticTokenColorsProvider.kt
@@ -16,15 +16,15 @@ class SlangSemanticTokenColorsProvider: SemanticTokensColorsProvider {
     {
         return when (tokenType) {
             "type" -> SlangTextAttributes.SLANG_TYPE_TEXT
-            "enumMember" -> createTextAttributesKey("Slang.LSP.enumMember", SlangTextAttributes.SLANG_ENUM_MEMBER_TEXT)
-            "variable" -> createTextAttributesKey("Slang.LSP.variable", SlangTextAttributes.SLANG_VARIABLE_TEXT)
-            "parameter" -> createTextAttributesKey("Slang.LSP.parameter", SlangTextAttributes.SLANG_PARAMETER_TEXT)
-            "function" -> createTextAttributesKey("Slang.LSP.function", SlangTextAttributes.SLANG_FUNCTION_DECL_TEXT)
-            "property" -> createTextAttributesKey("Slang.LSP.property", SlangTextAttributes.SLANG_INSTANCE_VARIABLE)
-            "namespace" -> createTextAttributesKey("Slang.LSP.namespace", SlangTextAttributes.SLANG_NAMESPACE_TEXT)
-            "keyword" -> createTextAttributesKey("Slang.LSP.keyword", SlangTextAttributes.SLANG_KEYWORDS_TEXT)
-            "macro" -> createTextAttributesKey("Slang.LSP.macro", SlangTextAttributes.SLANG_MACRO_TEXT)
-            "string" -> createTextAttributesKey("Slang.LSP.string", SlangTextAttributes.SLANG_STRING_LITERAL_TEXT)
+            "enumMember" -> SlangTextAttributes.SLANG_ENUM_MEMBER_TEXT
+            "variable" -> SlangTextAttributes.SLANG_VARIABLE_TEXT
+            "parameter" -> SlangTextAttributes.SLANG_PARAMETER_TEXT
+            "function" -> SlangTextAttributes.SLANG_FUNCTION_DECL_TEXT
+            "property" -> SlangTextAttributes.SLANG_INSTANCE_VARIABLE
+            "namespace" -> SlangTextAttributes.SLANG_NAMESPACE_TEXT
+            "keyword" -> SlangTextAttributes.SLANG_KEYWORDS_TEXT
+            "macro" -> SlangTextAttributes.SLANG_MACRO_TEXT
+            "string" -> SlangTextAttributes.SLANG_STRING_LITERAL_TEXT
             else -> null
         }
     }
