e04ad44212c145690a8fcee0304b66c8eb454c32
maxgodefroy <max@godefroy.net>
2024-12-25 21:30:51 +0100

    Json config put into service

--- src/main/kotlin/slang/plugin/lsp/intellij/SlangLspServerDescriptor.kt
+++ src/main/kotlin/slang/plugin/lsp/intellij/SlangLspServerDescriptor.kt
@@ -5,12 +5,14 @@ import com.intellij.openapi.fileTypes.FileTypeManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.lsp.api.LspServerDescriptor
+import org.eclipse.lsp4j.ConfigurationItem
 import slang.plugin.language.SlangFileType
+import slang.plugin.lsp.SlangJsonConfigService
 import slang.plugin.lsp.SlangLanguageServerProvider
 import kotlin.io.path.absolutePathString
 
 @Suppress("UnstableApiUsage")
-class SlangLspServerDescriptor(project: Project, val provider: SlangLanguageServerProvider)
+class SlangLspServerDescriptor(project: Project, private val provider: SlangLanguageServerProvider)
     : LspServerDescriptor(project, "Slang")
 {
     override fun createCommandLine(): GeneralCommandLine {
@@ -22,5 +24,18 @@ class SlangLspServerDescriptor(project: Project, val provider: SlangLanguageServ
 
     override fun isSupportedFile(file: VirtualFile): Boolean {
         return FileTypeManager.getInstance().getFileTypeByFile(file) is SlangFileType
+    }
+
+    override fun getWorkspaceConfiguration(item: ConfigurationItem): Any? {
+        val jsonConfig = SlangJsonConfigService.getInstance(project)
+
+        jsonConfig.prepareJsonConfig(project)
+
+        when (item.section) {
+            "slang.predefinedMacros",
+            "slang.additionalSearchPaths"
+                -> return jsonConfig.getEntry<List<String>>(item.section)
+        }
+        return super.getWorkspaceConfiguration(item)
     }
 }
--- src/main/kotlin/slang/plugin/lsp/lsp4ij/SlangLanguageClient.kt
+++ src/main/kotlin/slang/plugin/lsp/lsp4ij/SlangLanguageClient.kt
@@ -6,30 +6,18 @@ import com.google.gson.JsonParser
 import com.google.gson.JsonPrimitive
 import com.intellij.openapi.project.Project
 import com.redhat.devtools.lsp4ij.client.LanguageClientImpl
+import slang.plugin.lsp.SlangJsonConfigService
 import java.nio.file.Path
 
 class SlangLanguageClient(project: Project): LanguageClientImpl(project) {
 
     override fun createSettings(): Any? {
-        val path = Path.of(project.basePath ?: return null, "slang-config.json")
-        if (!path.toFile().exists())
-            return null
-        val json = JsonParser.parseString(path.toFile().readText(Charsets.UTF_8))
-
-        (((json as? JsonObject)?.get("slang") as? JsonObject)?.get("additionalSearchPaths") as? JsonArray)?.let { array ->
-            for (i in 0 until array.size()) {
-                val entry = array[i]!!
-                if (!entry.isJsonPrimitive || !entry.asJsonPrimitive.isString)
-                    continue
-
-                val entryPath = Path.of(entry.asJsonPrimitive.asString)
-
-                if (!entryPath.isAbsolute) {
-                    array.set(i, JsonPrimitive(Path.of(project.basePath!!, entryPath.toString()).toString()))
-                }
-            }
+        val jsonConfig = SlangJsonConfigService.getInstance(project)
+        jsonConfig.prepareJsonConfig(project)
+        jsonConfig.setUpdateListener {
+            triggerChangeConfiguration()
         }
-        return json
+        return jsonConfig.json
     }
 
 }
